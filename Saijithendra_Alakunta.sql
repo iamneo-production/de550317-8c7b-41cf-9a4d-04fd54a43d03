47ac4db1b387ca0fb66da3c97c2199dc8d3db9f0
Ajithendra <sjithendra73@gmail.com>
2023-07-30 18:07:20 +0000

    Saijithendra_Alakunta

--- Saijithendra_Alakunta.sql
+++ Saijithendra_Alakunta.sql
@@ -1,8 +1,5 @@
 set timing on;
 
-
-
-
 CREATE VIEW BANK_SYSTEM_VIEW AS
 SELECT EXTRACT(YEAR FROM "DATE") AS Year,
        CAST(REGEXP_REPLACE(WITHDRAWAL_AMT, '[^0-9.]', '') AS NUMBER) AS WithdrawalAmtCleaned
@@ -16,29 +13,21 @@ FROM BANK_TRANSACTION;
 /*CREATE INDEX WITHDRAW_INDEX ON 
 BANK_TRANSACTION ("DATE", WITHDRAWAL_AMT);*/
 
-
-
 -- Query 1: Highest Amount Debited per Year
-
 
 SELECT Year,
        MAX(WithdrawalAmtCleaned) AS HighestAmountDebited
 FROM BANK_SYSTEM_VIEW
 GROUP BY Year;
 
-
 -- Query 2: Lowest Amount Debited per Year
-
 
 SELECT Year,
        MIN(WithdrawalAmtCleaned) AS LowestAmountDebited
 FROM BANK_SYSTEM_VIEW
 GROUP BY Year;
 
-
-
 -- Query 3: Fifth Highest Amount Debited per Year
-
 
 SELECT Year, FifthHighestAmount
 FROM (
@@ -48,24 +37,16 @@ FROM (
 )
 WHERE R = 5;
 
-
-
 -- Query 4: Count of Withdrawal Transactions between May 5, 2018, and March 7, 2019
-
 
 SELECT COUNT(CAST(REGEXP_REPLACE(WITHDRAWAL_AMT, '[^0-9.]', '') AS NUMBER)) AS WithdrawalCount
 FROM WITHDRAW_R
 WHERE "DATE" BETWEEN TO_DATE('05-05-2018', 'DD-MM-YYYY') AND TO_DATE('07-03-2019', 'DD-MM-YYYY');
 
-
-
 -- Query 5: Top 5 Largest Withdrawal Amounts for the year 2018
-
 
 SELECT DISTINCT Year, WithdrawalAmtCleaned AS LargestWithdrawal
 FROM BANK_SYSTEM_VIEW
 WHERE Year = 2018
 ORDER BY WithdrawalAmtCleaned DESC
 FETCH FIRST 5 ROWS ONLY;
-
-
